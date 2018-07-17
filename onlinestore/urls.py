9a41ff7dcbbdc024df157b855057f44a022181e0
BorisVV <bcarlof@gmail.com>
2018-07-17 15:15:29 -0500

    redux v1.0 install django-redux, settings.py, urls.

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -21,4 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('onlineshop.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
