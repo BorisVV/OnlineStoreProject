678fd0f232ff389c265403e52df341ed568e4aa4
Momo Johnson <momo.johnson1987@gmail.com>
2018-07-17 14:29:20 +0000

    Restructure project to make it compatible with django 2.0

--- onlineshop/apps.py
+++ onlineshop/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class SellinggoodsConfig(AppConfig):
-    name = 'sellingGoods'
+    name = 'onlineshop'
--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('sellingGoods.urls')),
-]
+    path('admin/', admin.site.urls),
+    path('', include('onlineshop.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
